e393f61e43bd4802daac2615517ebaae163e8f8f
Zifeng Yuan <S01160737@acad.tri-c.edu>
2019-03-24 03:27:58 -0400

    Completed Lab 1

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -11,6 +11,7 @@ namespace Lab01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from [Zifeng Yuan]");
+            Console.ReadKey();
         }
     }
 }
